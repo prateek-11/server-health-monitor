77559ed31083259c026554e11b68791111b4fa76
Prateek Sahu cw <af546zz@3mhealth.com>
2026-04-21 20:23:09 +0530

    added sns alert logic

--- health_check.sh
+++ health_check.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-echo "DevOps pipeline test"
-echo "===== Server Health Report ====="
+CPU_USAGE=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}')
+CPU_INT=${CPU_USAGE%.*}
+THRESHOLD=50
 
-echo ""
-echo "Disk Usage"
-df -h
+ALERT_FILE="logs/alert_sent"
+TOPIC_ARN="arn:aws:sns:us-east-1:964242332531:cpu-alert-topic"
 
-echo ""
-echo "Memory Usage"
-free -m
+mkdir -p logs
 
-echo ""
-echo "CPU Usage"
-top -bn1 | grep "Cpu"
+echo "----- $(date) -----" >> logs/output.log
+echo "CPU usage: $CPU_INT%" >> logs/output.log
 
-echo ""
-echo "Top 5 Processes"
-ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head
+if [ "$CPU_INT" -gt "$THRESHOLD" ]; then
+  if [ ! -f "$ALERT_FILE" ]; then
+    echo "High CPU usage detected: $CPU_INT%" >> logs/output.log
+
+    aws sns publish \
+      --topic-arn $TOPIC_ARN \
+      --message "🚨 High CPU Alert: $CPU_INT%"
+
+    touch $ALERT_FILE
+  fi
+else
+  rm -f $ALERT_FILE
+fi
